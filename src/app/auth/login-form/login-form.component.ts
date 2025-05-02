a1c7d78e503b32dda9ec04efcb703ffdb7a98dd6
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-02 17:06:58 +0200

    feat(auth): implement reactive login form with validation and error handling

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,11 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, signal, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideGithub, lucideLoaderCircle } from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmErrorDirective } from '@spartan-ng/ui-formfield-helm';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HlmFormFieldModule } from '@spartan-ng/ui-formfield-helm';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@spartan-ng/brain/forms';
 
 @Component({
     selector: 'auth-login-form',
@@ -16,19 +21,56 @@ import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
     HlmInputDirective,
     FormsModule,
     HlmLabelDirective,
+    HlmErrorDirective,
+    ReactiveFormsModule,
+    HlmFormFieldModule,
+    HlmInputDirective,
 ],
     host: {
         class: 'block',
     },
-    providers: [provideIcons({ lucideGithub, lucideLoaderCircle })],
+    providers: [
+        provideIcons({ lucideGithub, lucideLoaderCircle }),
+        {
+            provide: ErrorStateMatcher,
+            useClass: ShowOnDirtyErrorStateMatcher
+        }
+    ],
     templateUrl: './login-form.component.html',
 })
-export class LoginFormComponent {
+export class LoginFormComponent  implements OnInit {
     public isLoading = signal(false);
+    loginForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            userEmail: ['', Validators.required],
+            userPassword: ['', Validators.required]
+        });
+    }
+    
 
     send() {
+        if (this.loginForm.invalid) {
+            // Mark all fields as touched to show errors
+            Object.keys(this.loginForm.controls).forEach(field => {
+                const control = this.loginForm.get(field);
+                control?.markAsTouched({ onlySelf: true });
+            });
+            return;
+        }
+
         this.isLoading.set(true);
         setTimeout(() => this.isLoading.set(false), 3000);
+
+        console.log('Login as:');
+        console.table({
+            email: this.loginForm.get('userEmail')?.value,
+            password: this.loginForm.get('userPassword')?.value,
+        });
+    }
+
+    ngOnInit(): void {
     }
 
 }
