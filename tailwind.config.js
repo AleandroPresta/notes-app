ecb3b50a092c38ed21ff0a244894c16c457b21ba
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-02 11:39:13 +0200

    Fixed tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('node:path');
+const path = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
+  presets: [
+    // Use path.resolve to point to the exact file location
+    require(path.resolve(__dirname, 'node_modules/@spartan-ng/brain/hlm-tailwind-preset.js'))
+  ],
   content: [
     join(__dirname, 'src/**/*.{html,ts}'),
     join(__dirname, 'libs/**/*.{html,ts}'),
