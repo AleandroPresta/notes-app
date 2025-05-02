cbef0104f3fa99fd13d2b6700b3cebacb53217e3
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-02 09:38:25 +0200

    Installed dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    AuthComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        AuthComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'notes-app';
