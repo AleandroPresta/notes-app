76514c32afadfcdcf6a4b78803b6dc0f9c2b5333
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-02 12:29:02 +0200

    feat(menu): implement hlm-menu components and integrate tabs functionality

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -5,9 +5,24 @@ import { HlmCardModule } from '@spartan-ng/ui-card-helm';
 import { SigninFormComponent } from './signin-form/signin-form.component';
 import { RouterOutlet } from '@angular/router';
 
+import {
+    HlmTabsComponent,
+    HlmTabsContentDirective,
+    HlmTabsListComponent,
+    HlmTabsTriggerDirective,
+} from '@spartan-ng/ui-tabs-helm';
+
 @Component({
 	selector: 'spartan-authentication',
-	imports: [SigninFormComponent, HlmButtonDirective, RouterOutlet],
+	imports: [
+		SigninFormComponent, 
+		HlmButtonDirective, 
+		RouterOutlet,
+		HlmTabsComponent,
+        HlmTabsListComponent,
+        HlmTabsTriggerDirective,
+        HlmTabsContentDirective,
+	],
 	templateUrl: './auth.component.html',
 })
 export class AuthComponent { }
