548da550a1e124b6b1c04f74b27192e4f2e1b289
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-02 12:56:25 +0200

    feat(auth): implement login form with validation and loading state

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideGithub, lucideLoaderCircle } from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { toast } from 'ngx-sonner';
+import { HlmToasterComponent } from "../../../../libs/ui/ui-sonner-helm/src/lib/hlm-toaster.component";
 
 @Component({
-  selector: 'auth-login-form',
-  imports: [],
-  templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+    selector: 'auth-login-form',
+    imports: [
+    HlmButtonDirective,
+    NgIcon,
+    HlmIconDirective,
+    HlmInputDirective,
+    FormsModule,
+    HlmLabelDirective,
+    HlmToasterComponent
+],
+    host: {
+        class: 'block',
+    },
+    providers: [provideIcons({ lucideGithub, lucideLoaderCircle })],
+    templateUrl: './login-form.component.html',
 })
 export class LoginFormComponent {
+    public isLoading = signal(false);
+
+    send() {
+        this.isLoading.set(true);
+        setTimeout(() => this.isLoading.set(false), 3000);
+    }
 
 }
