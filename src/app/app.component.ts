5a4a6506c5056974c2666964c898a967f13ffb0c
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-03 10:05:09 +0200

    fix(app): update app component template and logic for conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { AuthComponent } from './auth/auth.component';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    AuthComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [AuthComponent, NgIf],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'notes-app';
+    title = 'notes-app';
+
+    componentToShow = 'auth';
 }
