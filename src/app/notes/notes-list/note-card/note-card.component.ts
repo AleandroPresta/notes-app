9dcc7ce70a47be4f8924b216a00db80dc8832e1a
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-05 10:59:53 +0200

    refactor(note-card): simplify note initialization and remove mock data feat(notes-list): implement dynamic rendering of note cards

--- src/app/notes/notes-list/note-card/note-card.component.ts
+++ src/app/notes/notes-list/note-card/note-card.component.ts
@@ -65,21 +65,13 @@ import {
     styleUrl: './note-card.component.css',
 })
 export class NoteCardComponent {
-    @Input() protected note: Note = {
+    @Input() note: Note = {
         id: 0,
         title: '',
         content: '',
     };
 
-    constructor() {
-        // Create a mock note (TODO remove this in production)
-        this.note = {
-            id: 1,
-            title: 'Sample Note',
-            content:
-                'This is a sample note content. This is a sample note content. This is a sample note content. This is a sample note content. This is a sample note content.',
-        };
-    }
+    constructor() {}
 
     modifyNote() {
         // Logic to modify the note
