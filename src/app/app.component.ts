ce6d65f500eb4e29db3241346720b1d179dceff3
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-05 12:23:16 +0200

    Fixed space between footer and auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,18 @@ import { Component } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 import { NgIf } from '@angular/common';
 import { NotesComponent } from './notes/notes.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [AuthComponent, NgIf, NotesComponent],
+    imports: [AuthComponent, NgIf, NotesComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
     title = 'notes-app';
 
-    componentToShow = 'notes'; // Auth in production
+    componentToShow = 'auth'; // Auth in production
     userEmail = 'None';
 
     constructor() {}
