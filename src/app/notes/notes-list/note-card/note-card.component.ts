01a45192f26073e942c38c80832e33def4d8c8e8
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-05 10:57:00 +0200

    feat(tooltip): add HlmTooltip component and directive for enhanced user interaction

--- src/app/notes/notes-list/note-card/note-card.component.ts
+++ src/app/notes/notes-list/note-card/note-card.component.ts
@@ -2,17 +2,43 @@ import { Component, Input } from '@angular/core';
 import { Note } from '../../Notes';
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideEllipsisVertical } from '@ng-icons/lucide';
+import {
+    lucideEllipsisVertical,
+    lucidePen,
+    lucideTrash2,
+} from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import {
     HlmCardContentDirective,
     HlmCardDirective,
-    HlmCardFooterDirective,
     HlmCardHeaderDirective,
     HlmCardTitleDirective,
 } from '@spartan-ng/ui-card-helm';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
-import { HlmSwitchComponent } from '@spartan-ng/ui-switch-helm';
+
+import { BrnTooltipContentDirective } from '@spartan-ng/brain/tooltip';
+import {
+    HlmTooltipComponent,
+    HlmTooltipTriggerDirective,
+} from '@spartan-ng/ui-tooltip-helm';
+import { BrnMenuTriggerDirective } from '@spartan-ng/brain/menu';
+import { HlmMenuComponent } from '@spartan-ng/ui-menu-helm';
+import {
+    HlmMenuBarComponent,
+    HlmSubMenuComponent,
+    HlmMenuItemDirective,
+    HlmMenuItemSubIndicatorComponent,
+    HlmMenuLabelComponent,
+    HlmMenuShortcutComponent,
+    HlmMenuSeparatorComponent,
+    HlmMenuItemIconDirective,
+    HlmMenuBarItemDirective,
+    HlmMenuItemCheckComponent,
+    HlmMenuItemRadioComponent,
+    HlmMenuGroupComponent,
+    HlmMenuItemCheckboxDirective,
+    HlmMenuItemRadioDirective,
+} from '@spartan-ng/ui-menu-helm';
 
 @Component({
     selector: 'spartan-note-card',
@@ -25,8 +51,16 @@ import { HlmSwitchComponent } from '@spartan-ng/ui-switch-helm';
         HlmCardTitleDirective,
         HlmIconDirective,
         NgIcon,
+        BrnMenuTriggerDirective,
+
+        HlmMenuComponent,
+        HlmMenuBarComponent,
+        HlmMenuItemDirective,
+        HlmMenuBarItemDirective,
     ],
-    providers: [provideIcons({ lucideEllipsisVertical })],
+    providers: [
+        provideIcons({ lucideEllipsisVertical, lucideTrash2, lucidePen }),
+    ],
     templateUrl: './note-card.component.html',
     styleUrl: './note-card.component.css',
 })
@@ -45,5 +79,14 @@ export class NoteCardComponent {
             content:
                 'This is a sample note content. This is a sample note content. This is a sample note content. This is a sample note content. This is a sample note content.',
         };
+    }
+
+    modifyNote() {
+        // Logic to modify the note
+        console.log('Modify note:', this.note);
+    }
+    deleteNote() {
+        // Logic to delete the note
+        console.log('Delete note:', this.note);
     }
 }
