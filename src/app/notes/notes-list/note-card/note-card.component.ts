a41e1086ca56311e8740a9f1b1ab975219ca5e26
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-05 10:27:52 +0200

    Completed basic note component

--- src/app/notes/notes-list/note-card/note-card.component.ts
+++ src/app/notes/notes-list/note-card/note-card.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Note } from '../../Notes';
 
-import { provideIcons } from '@ng-icons/core';
-import { lucideBell, lucideCheck } from '@ng-icons/lucide';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideEllipsisVertical } from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import {
     HlmCardContentDirective,
-    HlmCardDescriptionDirective,
     HlmCardDirective,
     HlmCardFooterDirective,
     HlmCardHeaderDirective,
@@ -21,15 +20,13 @@ import { HlmSwitchComponent } from '@spartan-ng/ui-switch-helm';
     imports: [
         HlmButtonDirective,
         HlmCardContentDirective,
-        HlmCardDescriptionDirective,
         HlmCardDirective,
-        HlmCardFooterDirective,
         HlmCardHeaderDirective,
         HlmCardTitleDirective,
         HlmIconDirective,
-        HlmSwitchComponent,
+        NgIcon,
     ],
-    providers: [provideIcons({ lucideCheck, lucideBell })],
+    providers: [provideIcons({ lucideEllipsisVertical })],
     templateUrl: './note-card.component.html',
     styleUrl: './note-card.component.css',
 })
@@ -39,24 +36,6 @@ export class NoteCardComponent {
         title: '',
         content: '',
     };
-
-    protected notifications = [
-        {
-            id: 1,
-            title: 'Your call has been confirmed.',
-            description: '1 hour ago',
-        },
-        {
-            id: 2,
-            title: 'You have a new message!',
-            description: '1 hour ago',
-        },
-        {
-            id: 3,
-            title: 'Your subscription is expiring soon!',
-            description: '2 hours ago',
-        },
-    ];
 
     constructor() {
         // Create a mock note (TODO remove this in production)
--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -5,7 +5,7 @@ import { NoteCardComponent } from './note-card/note-card.component';
 
 @Component({
     selector: 'app-notes-list',
-    imports: [NgFor, NoteCardComponent],
+    imports: [NoteCardComponent],
     templateUrl: './notes-list.component.html',
     styleUrl: './notes-list.component.css',
 })
