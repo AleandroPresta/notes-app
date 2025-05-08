714ced21195738a9e397d461013d15f0b626e4a4
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-08 17:28:59 +0200

    Refactor Sentry initialization and add mock localStorage for SSR environments

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import * as Sentry from "@sentry/angular";
+import * as Sentry from '@sentry/angular';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject } from '@angular/core';
 
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// Create a mock localStorage for SSR environments
+if (typeof window === 'undefined') {
+    const mockLocalStorage = {
+        getItem: () => null,
+        setItem: () => null,
+        removeItem: () => null,
+        clear: () => null,
+        key: () => null,
+        length: 0,
+    };
+
+    // @ts-ignore - Add global mock for SSR
+    global.localStorage = mockLocalStorage;
+}
+
 Sentry.init({
-  dsn: "https://2fa6ddeddbc1606ee5004d199f73d8df@o4509287929872384.ingest.de.sentry.io/4509287931183184",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true
+    dsn: 'https://2fa6ddeddbc1606ee5004d199f73d8df@o4509287929872384.ingest.de.sentry.io/4509287931183184',
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true,
+    // Only initialize in browser environments
+    enabled: typeof window !== 'undefined',
 });
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error(err)
+);
