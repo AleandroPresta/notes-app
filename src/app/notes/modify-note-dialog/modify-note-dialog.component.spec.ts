69a83bbbe5dd12d0172c52774e7c93771a8c629a
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-08 17:13:57 +0200

    Refactor ModifyNoteDialogComponent tests to include FormsModule and CommonModule imports

--- src/app/notes/modify-note-dialog/modify-note-dialog.component.spec.ts
+++ src/app/notes/modify-note-dialog/modify-note-dialog.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { ModifyNoteDialogComponent } from './modify-note-dialog.component';
+import { NotesService } from '../notes.service';
+import { Note } from '../Note';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 describe('ModifyNoteDialogComponent', () => {
-  let component: ModifyNoteDialogComponent;
-  let fixture: ComponentFixture<ModifyNoteDialogComponent>;
+    let component: ModifyNoteDialogComponent;
+    let fixture: ComponentFixture<ModifyNoteDialogComponent>;
+    let mockNotesService: jasmine.SpyObj<NotesService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ModifyNoteDialogComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        // Create mock for NotesService
+        mockNotesService = jasmine.createSpyObj('NotesService', ['updateNote']);
+        mockNotesService.updateNote.and.returnValue(
+            of({ id: 1, title: 'Updated Note', content: 'Updated content' })
+        );
 
-    fixture = TestBed.createComponent(ModifyNoteDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            imports: [ModifyNoteDialogComponent, FormsModule, CommonModule],
+            providers: [{ provide: NotesService, useValue: mockNotesService }],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(ModifyNoteDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
